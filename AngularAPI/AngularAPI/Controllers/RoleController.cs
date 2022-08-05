3bffccb25936f53ea69e48724b77d8606146cdc4
Nada_Kohya <94925501+NadaKohya@users.noreply.github.com>
2022-08-05 18:36:00 +0200

    :)

---
+++ AngularAPI/AngularAPI/Controllers/RoleController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AngularAPI.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AngularAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class RoleController : Controller
+    {
+        private readonly RoleManager<IdentityRole> roleManager;
+        public RoleController(RoleManager<IdentityRole> roleManager)
+        {
+            this.roleManager = roleManager;
+        }
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Role newRole)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityRole role = new IdentityRole();
+                role.Name = newRole.Name;
+                IdentityResult result = await roleManager.CreateAsync(role);
+                if (result.Succeeded)
+                {
+                    return Ok();
+                }
+                else
+                {
+                    foreach (var item in result.Errors)
+                    {
+                        ModelState.AddModelError("", item.Description);
+                    }
+                }
+            }
+            return BadRequest(ModelState);
+        }
+    }
+}
+
---
+++ AngularAPI/AngularAPI/Models/Role.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AngularAPI.Models
+{
+	public class Role
+	{
+		[Required]
+		public string Name { get; set; }
+	}
+}
+
