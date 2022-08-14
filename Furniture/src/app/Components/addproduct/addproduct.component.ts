f267650448e10da25a80c7ef1ee27bef226178a6
Nada_Kohya <94925501+NadaKohya@users.noreply.github.com>
2022-08-14 04:39:25 +0200

    new category, new product and changes in footer dimensions

--- Furniture/src/app/Components/addproduct/addproduct.component.ts
+++ Furniture/src/app/Components/addproduct/addproduct.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CategoryService } from 'src/app/Services/category.service';
 
 @Component({
   selector: 'app-addproduct',
@@ -6,10 +8,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./addproduct.component.css']
 })
 export class AddproductComponent implements OnInit {
+  produuctId: any;
+  product: any;
+  image1: any;
+  image2: any;
+  image3: any;
+  categoryId: any;
+  categories: any;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private activited: ActivatedRoute,
+    private myCategoryService: CategoryService
+  ) {
+    this.produuctId = activited.snapshot.params['id'];
   }
 
+  ngOnInit(): void {
+    this.myCategoryService.getAllCategories().subscribe(
+      (data) => {
+        this.categories = data;
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+  }
+  OnSelect(event: any) {
+    this.image1 = event.target.files[0].name;
+  }
+
+  changeCat(e: any) {
+    this.categoryId = e.target.value;
+    console.log(this.categoryId);
+  }
 }
